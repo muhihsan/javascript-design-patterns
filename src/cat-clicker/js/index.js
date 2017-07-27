302a5414c31d76863c638e27539cc728e986587b
Muhammad Ihsan <mihsan191@gmail.com>
2017-07-27 10:53:32 +1000

    Show 2 cats, where each will have name, picture and num of clicks

--- src/cat-clicker/js/index.js
+++ src/cat-clicker/js/index.js
@@ -1,23 +1,34 @@
 (function(){
 
-  var count = {
-    numOfClicked: 0,
-    incrementNumOfClick: function() {
-      this.numOfClicked++;
-    },
-    showNumOfClick: function(elem) {
-      if (elem !== null) {
-        elem.innerHTML = this.numOfClicked + ' time' + (this.numOfClicked === 1? '' : 's') + ' clicked.';
+  function cat(id, catName) {
+    var self = this;
+    self.element = document.getElementById(id);
+    self.catName = catName;
+    self.numOfClicked = 0;
+
+    self.incrementNumOfClick = function() {
+      self.numOfClicked++;
+    };
+
+    self.showNumOfClick = function() {
+      var eNumOfClick = self.element.getElementsByClassName('num-of-click')[0];
+      if (eNumOfClick !== null) {
+        eNumOfClick.innerHTML = self.numOfClicked + ' time' + (self.numOfClicked === 1? '' : 's') + ' clicked.';
       }
-    }
+    };
+
+    self.element.getElementsByClassName('cat-name')[0].innerHTML = self.catName;
+    self.showNumOfClick();
+
+    var eCatImage = self.element.getElementsByClassName('cat-image')[0];
+
+    eCatImage.addEventListener('click', function(e) {
+      self.incrementNumOfClick();
+      self.showNumOfClick();
+    }, false);
   }
 
-  var elem = document.getElementById('num-of-click');
-  count.showNumOfClick(elem);
-
-  document.getElementById('cat-image').addEventListener('click', function(e) {
-    count.incrementNumOfClick();
-    count.showNumOfClick(elem);
-  }, false);
+  var firstCat = new cat('first-cat', 'Daenerys Targaryen');
+  var secondCat = new cat('second-cat', 'John Snow');
 
 })()
