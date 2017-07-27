e0f37bf196545ceb99d714de2ce7ae7ac64a773a
Muhammad Ihsan <mihsan191@gmail.com>
2017-07-27 12:11:22 +1000

    Show 5 list of cat, and show cat info when it's clicked

--- src/cat-clicker/js/index.js
+++ src/cat-clicker/js/index.js
@@ -1,9 +1,8 @@
 (function(){
 
-  function cat(id, catName) {
+  function cat(catName) {
     var self = this;
-    self.element = document.getElementById(id);
-    self.catName = catName;
+    self.name = catName;
     self.numOfClicked = 0;
 
     self.incrementNumOfClick = function() {
@@ -11,24 +10,56 @@
     };
 
     self.showNumOfClick = function() {
-      var eNumOfClick = self.element.getElementsByClassName('num-of-click')[0];
+      var eNumOfClick = document.getElementsByClassName('num-of-click')[0];
       if (eNumOfClick !== null) {
         eNumOfClick.innerHTML = self.numOfClicked + ' time' + (self.numOfClicked === 1? '' : 's') + ' clicked.';
       }
     };
 
-    self.element.getElementsByClassName('cat-name')[0].innerHTML = self.catName;
-    self.showNumOfClick();
+    self.show = function() {
+      var elemName = document.createElement('h2');
+      elemName.className = 'text-center cat-name';
+      elemName.innerText = self.name;
+      var elemImage = document.createElement('img');
+      elemImage.className = 'img-responsive cursor-pointer cat-image';
+      elemImage.setAttribute('src', 'http://placekitten.com/1500/400');
+      elemImage.setAttribute('alt', "Cat " + self.name);
+      elemImage.setAttribute('title', 'Click me to add the click count');
+      var elemNumOfClick = document.createElement('h3');
+      elemNumOfClick.className = 'text-center num-of-click';
 
-    var eCatImage = self.element.getElementsByClassName('cat-image')[0];
+      var container = document.getElementById('cat-show');
+      container.innerHTML = '';
+      container.appendChild(elemName);
+      container.appendChild(elemImage);
+      container.appendChild(elemNumOfClick);
 
-    eCatImage.addEventListener('click', function(e) {
-      self.incrementNumOfClick();
       self.showNumOfClick();
+
+      var eCatImage = container.getElementsByClassName('cat-image')[0];
+      eCatImage.addEventListener('click', function(e) {
+        self.incrementNumOfClick();
+        self.showNumOfClick();
+      }, false);
+    }
+
+    var elemCat = document.createElement('li');
+    var elemCatLink = document.createElement('a');
+    elemCatLink.className = 'cursor-pointer';
+    elemCatLink.id = self.name;
+    elemCatLink.setAttribute('atr', '#');
+    elemCatLink.innerText = self.name;
+    elemCatLink.addEventListener('click', function(e) {
+      self.show();
     }, false);
+    elemCat.appendChild(elemCatLink);
+    var elemCatList = document.getElementById('cat-list');
+    elemCatList.appendChild(elemCat);
   }
 
-  var firstCat = new cat('first-cat', 'Daenerys Targaryen');
-  var secondCat = new cat('second-cat', 'John Snow');
-
+  var firstCat = new cat('Daenerys Targaryen');
+  var secondCat = new cat('John Snow');
+  var secondCat = new cat('Roberth Baratheon');
+  var secondCat = new cat('Tyrion Lannister');
+  var secondCat = new cat('Ned Stark');
 })()
