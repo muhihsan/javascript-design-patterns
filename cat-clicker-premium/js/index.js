80c99a87e818724378afd1c1a93b73131627adc2
Muhammad Ihsan <mihsan191@gmail.com>
2017-07-28 09:58:22 +1000

    Add view object for admin, use self to make reference to this, show admin form when button clicked

--- cat-clicker-premium/js/index.js
+++ cat-clicker-premium/js/index.js
@@ -7,41 +7,43 @@
     self.numOfClick = 0;
   };
 
-  var model = {
+  var catModel = {
     cats: [],
     selectedCat: null,
 
     init: function() {
-      this.cats.push(new Cat('Daenerys Targryen', 'https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg'));
-      this.cats.push(new Cat('John Snow', 'https://static.pexels.com/photos/33537/cat-animal-cat-portrait-mackerel.jpg'));
-      this.cats.push(new Cat('Robert Baratheon', 'https://static.pexels.com/photos/511041/pexels-photo-511041.jpeg'));
-      this.cats.push(new Cat('Tyrion Lannister', 'https://static.pexels.com/photos/156934/pexels-photo-156934.jpeg'));
-      this.cats.push(new Cat('Ned Stark', 'https://static.pexels.com/photos/86243/pexels-photo-86243.jpeg'));
-      this.selectedCat = this.cats[0];
+      var self = this;
+      self.cats.push(new Cat('Daenerys Targryen', 'https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg'));
+      self.cats.push(new Cat('John Snow', 'https://static.pexels.com/photos/33537/cat-animal-cat-portrait-mackerel.jpg'));
+      self.cats.push(new Cat('Robert Baratheon', 'https://static.pexels.com/photos/511041/pexels-photo-511041.jpeg'));
+      self.cats.push(new Cat('Tyrion Lannister', 'https://static.pexels.com/photos/156934/pexels-photo-156934.jpeg'));
+      self.cats.push(new Cat('Ned Stark', 'https://static.pexels.com/photos/86243/pexels-photo-86243.jpeg'));
+      self.selectedCat = self.cats[0];
     }
   };
 
   var octopus = {
     init: function() {
-      model.init();
+      catModel.init();
       catListView.init();
       catView.init();
+      adminView.init();
     },
 
     getAllCats: function() {
-      return model.cats;
+      return catModel.cats;
     },
 
     getSelectedCat: function() {
-      return model.selectedCat;
+      return catModel.selectedCat;
     },
 
     selectCat: function(cat) {
-      model.selectedCat = cat;
+      catModel.selectedCat = cat;
     },
 
     addNumOfClick: function() {
-      model.selectedCat.numOfClick++;
+      catModel.selectedCat.numOfClick++;
     }
   };
 
@@ -71,27 +73,62 @@
 
   var catView = {
     init: function() {
-      this.elemName = document.getElementById('cat-name');
-      this.elemImage = document.getElementById('cat-image');
-      this.elemNumOfClick = document.getElementById('num-of-click');
+      var self = this;
+      self.elemName = document.getElementById('cat-name');
+      self.elemImage = document.getElementById('cat-image');
+      self.elemNumOfClick = document.getElementById('num-of-click');
 
-      this.elemImage.addEventListener('click', function(e) {
+      self.elemImage.addEventListener('click', function(e) {
         octopus.addNumOfClick();
         catView.render();
+      }, false);
+
+      self.render();
+    },
+
+    render: function() {
+      var self = this;
+      var cat = octopus.getSelectedCat();
+
+      self.elemName.innerText = cat.name;
+      self.elemImage.setAttribute('alt', "Cat " + cat.name);
+      self.elemImage.setAttribute('src', cat.url);
+      self.elemNumOfClick.innerHTML = cat.numOfClick + ' time' + (cat.numOfClick === 1? '' : 's') + ' clicked.';
+    }
+  };
+
+  var adminView = {
+    init: function() {
+      var self = this;
+      self.elemAdminAccessButton = document.getElementById('admin-access');
+      self.elemAdminActionForm = document.getElementById('admin-action');
+      self.elemNameForm = document.getElementById('cat-name-form');
+      self.elemUrlForm = document.getElementById('cat-image-url-form');
+      self.elemNumOfClickForm = document.getElementById('cat-num-of-click-form');
+      self.elemSaveEditButton = document.getElementById('save-edit');
+      self.elemCancelEditButton = document.getElementById('cancel-edit');
+
+      self.elemAdminAccessButton.addEventListener('click', function() {
+        self.show();
       }, false);
 
       this.render();
     },
 
     render: function() {
+      var self = this;
       var cat = octopus.getSelectedCat();
+      self.elemNameForm.value = cat.name;
+      self.elemUrlForm = cat.url;
+      self.elemNumOfClickForm = cat.numOfClick;
+    },
 
-      this.elemName.innerText = cat.name;
-      this.elemImage.setAttribute('alt', "Cat " + cat.name);
-      this.elemImage.setAttribute('src', cat.url);
-      this.elemNumOfClick.innerHTML = cat.numOfClick + ' time' + (cat.numOfClick === 1? '' : 's') + ' clicked.';
+    show: function() {
+      var self = this;
+      self.elemAdminActionForm.className = '';
+      self.elemAdminAccessButton.className = self.elemAdminAccessButton.className + ' disabled';
     }
-  };
+  }
 
   octopus.init();
 })()
