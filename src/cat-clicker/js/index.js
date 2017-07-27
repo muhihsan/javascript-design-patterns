7811527da21481a07b2d5ac8ffa44a1374d28659
Muhammad Ihsan <mihsan191@gmail.com>
2017-07-27 10:19:53 +1000

    Move clicked assignment to javascript

--- src/cat-clicker/js/index.js
+++ src/cat-clicker/js/index.js
@@ -7,7 +7,7 @@
     },
     showNumOfClick: function(elem) {
       if (elem !== null) {
-        elem.innerHTML = this.numOfClicked + " time" + (this.numOfClicked === 1? "" : "s");
+        elem.innerHTML = this.numOfClicked + " time" + (this.numOfClicked === 1? "" : "s") + " clicked.";
       }
     }
   }
