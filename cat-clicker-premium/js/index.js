cf2884175bdf13ff3f04ef0ae1519efb48c6c6ca
Muhammad Ihsan <mihsan191@gmail.com>
2017-07-28 14:48:53 +1000

    Allow saving update to cat object

--- cat-clicker-premium/js/index.js
+++ cat-clicker-premium/js/index.js
@@ -1,10 +1,10 @@
 (function(){
 
-  var Cat = function(catName, url) {
+  var Cat = function(catName, url, numOfClick) {
     var self = this;
     self.name = catName;
     self.url = url;
-    self.numOfClick = 0;
+    self.numOfClick = numOfClick;
   };
 
   var catModel = {
@@ -13,11 +13,11 @@
 
     init: function() {
       var self = this;
-      self.cats.push(new Cat('Daenerys Targryen', 'https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg'));
-      self.cats.push(new Cat('John Snow', 'https://static.pexels.com/photos/33537/cat-animal-cat-portrait-mackerel.jpg'));
-      self.cats.push(new Cat('Robert Baratheon', 'https://static.pexels.com/photos/511041/pexels-photo-511041.jpeg'));
-      self.cats.push(new Cat('Tyrion Lannister', 'https://static.pexels.com/photos/156934/pexels-photo-156934.jpeg'));
-      self.cats.push(new Cat('Ned Stark', 'https://static.pexels.com/photos/86243/pexels-photo-86243.jpeg'));
+      self.cats.push(new Cat('Daenerys Targryen', 'https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg', 0));
+      self.cats.push(new Cat('John Snow', 'https://static.pexels.com/photos/33537/cat-animal-cat-portrait-mackerel.jpg', 0));
+      self.cats.push(new Cat('Robert Baratheon', 'https://static.pexels.com/photos/511041/pexels-photo-511041.jpeg', 0));
+      self.cats.push(new Cat('Tyrion Lannister', 'https://static.pexels.com/photos/156934/pexels-photo-156934.jpeg', 0));
+      self.cats.push(new Cat('Ned Stark', 'https://static.pexels.com/photos/86243/pexels-photo-86243.jpeg', 0));
       self.selectedCat = self.cats[0];
     }
   };
@@ -44,11 +44,24 @@
 
     addNumOfClick: function() {
       catModel.selectedCat.numOfClick++;
+    },
+
+    saveCat: function(updatedCat) {
+      var selectedCat = this.getSelectedCat();
+      selectedCat.name = updatedCat.name;
+      selectedCat.url = updatedCat.url;
+      selectedCat.numOfClick = updatedCat.numOfClick;
     }
   };
 
   var catListView = {
     init: function() {
+      this.render();
+    },
+
+    render: function() {
+      var elemCatList = document.getElementById('cat-list');
+      elemCatList.innerHTML = '';
       octopus.getAllCats().forEach(function(cat) {
         var elemCat = document.createElement('li');
         var elemCatLink = document.createElement('a');
@@ -65,7 +78,6 @@
         })(cat), false);
 
         elemCat.appendChild(elemCatLink);
-        var elemCatList = document.getElementById('cat-list');
         elemCatList.appendChild(elemCat);
       });
     }
@@ -116,19 +128,22 @@
         self.hide();
       }, false);
 
-      this.render();
+      self.elemSaveEditButton.addEventListener('click', function() {
+        self.save();
+      }, false);
     },
 
     render: function() {
       var self = this;
       var cat = octopus.getSelectedCat();
       self.elemNameForm.value = cat.name;
-      self.elemUrlForm = cat.url;
-      self.elemNumOfClickForm = cat.numOfClick;
+      self.elemUrlForm.value = cat.url;
+      self.elemNumOfClickForm.value = cat.numOfClick;
     },
 
     show: function() {
       var self = this;
+      self.render();
       self.elemAdminActionForm.className = '';
       self.elemAdminAccessButton.className = self.elemAdminAccessButton.className + ' disabled';
     },
@@ -137,6 +152,18 @@
       var self = this;
       self.elemAdminActionForm.className = 'hidden';
       self.elemAdminAccessButton.className = self.elemAdminAccessButton.className.replace(' disabled', '');
+    },
+
+    save: function() {
+      var self = this;
+      var name = self.elemNameForm.value;
+      var url = self.elemUrlForm.value;
+      var numOfClick = self.elemNumOfClickForm.value;
+      var updatedCat = new Cat(name, url, numOfClick);
+      octopus.saveCat(updatedCat);
+      catListView.render();
+      catView.render();
+      self.hide();
     }
   }
 
